d51dd31e8769cb065c47571e8ec3e206b8d91409
Maitrik Patel <59853047+maitrikpatel2025@users.noreply.github.com>
2022-11-25 23:03:47 -0500

    new-tickets

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,40 +1,38 @@
-
 import { useState } from "react";
 import useRequest from "../../hooks/use-request";
-import  Router  from "next/router";
-
+import Router from "next/router";
 
 const NewTicket = () => {
-const [title, setTitle] = useState("");
-const [price, setPrice] = useState("");
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
 
-const { doRequest, errors } = useRequest({
-  url: '/api/tickets',
-  method: 'post',
-  body: {
-    title,
-    price,
-  },
-  onSuccess: () => Router.push('/'),
-});
+  const { doRequest, errors } = useRequest({
+    url: "/api/tickets",
+    method: "post",
+    body: {
+      title,
+      price,
+    },
+    onSuccess: () => Router.push("/"),
+  });
 
-const onSubmit = async (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
     doRequest();
   };
-const onBlur = async () =>{
-    const value = parseFloat(price)
-    if (isNaN(value)){
-        return
+  const onBlur = async () => {
+    const value = parseFloat(price);
+    if (isNaN(value)) {
+      return;
     }
-    setPrice(value.toFixed(2))
-}
+    setPrice(value.toFixed(2));
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
         <div className="justify-content-center p-2 shadow-lg  m-5 bg-white rounded">
-          <h2 className="col d-flex justify-content-center text-dark ">
-           Create A New Ticket
+          <h2 className="col d-flex justify-content-center text-dark border rounded border-2 m-2 p-3">
+            Create A New Ticket
           </h2>
         </div>
         <div className="justify-content-center  p-5 shadow-lg p-3 m-5 bg-white rounded">
@@ -63,11 +61,13 @@ const onBlur = async () =>{
           <div className="row mb-4">
             <div className="col d-flex justify-content-center"></div>
           </div>
-            {errors}
-          <button className="btn btn-dark btn-block"> <i className="bi bi-plus-square"></i> Create</button>
+          {errors}
+          <button className="btn btn-dark btn-block">
+            {" "}
+            <i className="bi bi-plus-square"></i> Create
+          </button>
         </div>
       </form>
-    
     </div>
   );
 };
